8728feb5d34455382dd3ca857f528cb1f85e0933
SachinHanda-26 <sachinhanda8349@gmail.com>
2025-12-31 22:59:11 +0530

    exploring GET, POST && DELETE

--- src/app.js
+++ src/app.js
@@ -2,17 +2,39 @@ const express = require('express');
 
 const app = express();
 
+// app.use("/hello/123", (req, res)=>{
+//   res.send("Hello from hello 123 route");
+// })
+
+// app.use("/hello", (req, res)=>{
+//     res.send("Hello from hello route");
+// })
+
+
+// app.use("/user", (req, res)=>{
+//   res.send("HAHAHAHAHA");
+// })
+
+app.get("/user", (req, res)=>{
+  res.send({firstname: "Sachin", secondname: "Handa"});
+})
+
+app.post("/user", (req, res)=>{
+  res.send("Data saved successfully to DataBase.");
+})
+
+app.delete("/user", (req, res)=>{
+  res.send("Data Deleted Successfully from DataBase");
+})
+// this will match all HTTP methods API call to /test route
 app.use("/test", (req, res)=>{
     res.send("Hello from test route");
 })
 
-app.use("/hello", (req, res)=>{
-    res.send("Hello from hello route");
-})
 
-app.use("/",(req, res)=>{
-res.send("Hello from dashboard");
-})
+// app.use("/",(req, res)=>{
+// res.send("Hello from dashboard");
+// })
 
 
 app.listen(7777, ()=>{
